71f968ce0add5126e60fc713d4e96c94f3a20491
leo123nunes <ti.userleo@hotmail.com>
2020-09-24 18:52:45 -0300

    Criando classe pessoa e testando calculo de imc

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
-import Pessoa.Pessoa
+import Entidades.Pessoa
 import com.sun.xml.internal.ws.client.ContentNegotiation
 import io.ktor.application.call
 import io.ktor.application.install
@@ -13,30 +13,12 @@ fun main() {
 
     embeddedServer(Netty,8080){
         routing{
+            var mateus = Pessoa("Matheus",20,150.5, 1.75)
+            mateus.calcularIMC()
+
             get("/"){
-                call.respondText("<h1> Testando </h1>", ContentType.Text.Html)
+                call.respondText("<h1> ${mateus.calcularIMC()} </h1>", ContentType.Text.Html)
             }
         }
     }.start(wait = true)
-
-    var Mateus = Pessoa()
-
-    Mateus.nome = "Matheus"
-    Mateus.altura = 1.75
-    Mateus.peso = 150.5
-
-    imc(Mateus.peso, Mateus.altura)
-}
-
-fun imc(peso: Double, altura: Double) {
-    var imc: Double = peso / (altura * altura)
-    println("O imc da pessoa é $imc")
-    when {
-        imc < 18.5 -> println("Você está abaixo do peso ideal.")
-        imc >= 18.5 && imc < 25 -> println("Você está no peso ideal.")
-        imc >= 25 && imc < 30 -> println("Você está em pré-obesidade.")
-        imc >= 30 && imc < 35 -> println("Você está em besidade de grau 1.")
-        imc >= 35 && imc < 40 -> println("Você está em besidade de grau 2.")
-        else -> println("Você está em besidade mórbida.")
-    }
 }
