e2e280bff378a399f94830fa89c483ec39b0feb3
leo123nunes <ti.userleo@hotmail.com>
2020-09-24 18:32:44 -0300

    Adicionando dependencias do ktor, logback e gson, e criando servidor na porta 8080

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,24 @@
 import Pessoa.Pessoa
+import com.sun.xml.internal.ws.client.ContentNegotiation
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main() {
+
+    embeddedServer(Netty,8080){
+        routing{
+            get("/"){
+                call.respondText("<h1> Testando </h1>", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+
     var Mateus = Pessoa()
 
     Mateus.nome = "Matheus"
